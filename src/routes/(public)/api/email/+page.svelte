843f544341ff507f37baa675d6ea70c75dd409c9
yeehow-eg <yeehow@eurogain.com.my>
2023-05-14 19:19:22 +0800

    updated sent email design

--- src/routes/(public)/api/email/+page.svelte
+++ src/routes/(public)/api/email/+page.svelte
@@ -1,6 +1,13 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    export let data;
+  import { Button } from "carbon-components-svelte";
 </script>
 
-Sending...
+<div class="p-5">
+  <div class="p-8 bg-white">
+    <h1 class="text-2xl">Thank you for contacting us</h1>
+    <p class="text-lg text-gray-600 mb-10">We'll get back to you soon</p>
+    <div>
+      <Button href="/">Back to home</Button>
+    </div>
+  </div>
+</div>
