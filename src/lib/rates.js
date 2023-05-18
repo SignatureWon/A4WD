52722304b75c4c699f43bf428a18270e12840de3
yeehow-eg <yeehow@eurogain.com.my>
2023-05-18 11:27:33 +0800

    fixed vehicle search rates

---
+++ src/lib/rates.js
@@ -0,0 +1,11 @@
+import dayjs from "dayjs";
+import isBetween from "dayjs/plugin/isBetween";
+dayjs.extend(isBetween);
+import { supabase } from "$lib/supabaseClient";
+
+export const cal = {
+    getRates: (search) => {
+
+    },
+}
+  
