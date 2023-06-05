7c8f39a47eb9735d11aa94614ba17c5af9a3c1e7
yeehow-eg <yeehow@eurogain.com.my>
2023-06-05 15:11:16 +0800

    updated label

--- src/lib/components/admin/LayoutAsideContent.svelte
+++ src/lib/components/admin/LayoutAsideContent.svelte
@@ -74,13 +74,13 @@
       },
       {
         key: "manual_quotations",
-        name: "Quotations",
+        name: "Manual Quote",
         link: "/admin/sales/manual_quotations",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="w-5 h-5" fill="currentColor"><path fill="none" d="M0 0h24v24H0z"/><path d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zM5.003 8L5 20h10V8H5.003zM9 6h8v10h2V4H9v2z"/></svg>`,
       },
       {
         key: "manual_bookings",
-        name: "Bookings",
+        name: "Manual Booking",
         link: "/admin/sales/manual_bookings",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="w-5 h-5" fill="currentColor"><path fill="none" d="M0 0h24v24H0z"/><path d="M17 16h2V4H9v2h8v10zm0 2v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3zM5.003 8L5 20h10V8H5.003zM7 16h4.5a.5.5 0 1 0 0-1h-3a2.5 2.5 0 1 1 0-5H9V9h2v1h2v2H8.5a.5.5 0 1 0 0 1h3a2.5 2.5 0 1 1 0 5H11v1H9v-1H7v-2z"/></svg>`,
       },
