f431bbf4616cb26d8889fc4610cbe0770fee6a8f
yeehow-eg <yeehow@eurogain.com.my>
2023-06-06 10:52:56 +0800

    test chromium min executablePath

--- src/routes/admin/sales/final/[id]/+page.server.js
+++ src/routes/admin/sales/final/[id]/+page.server.js
@@ -172,7 +172,7 @@ export const actions = {
     const browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(),
+      executablePath: await chromium.executablePath("/opt/chromium"),
       headless: chromium.headless,
     });
     const page = await browser.newPage();
