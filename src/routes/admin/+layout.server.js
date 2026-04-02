87aa9c004dcf5c4a789fa4fdfcc83cd0ef77292e
yeehow-eg <yeehow@eurogain.com.my>
2026-04-02 18:04:37 +0800

    fixed admin session issue

--- src/routes/admin/+layout.server.js
+++ src/routes/admin/+layout.server.js
@@ -1,7 +1,14 @@
 import { getServerSession } from '@supabase/auth-helpers-sveltekit'
+import { redirect } from "@sveltejs/kit";
 
 export const load = async (event) => {
+  const session = await getServerSession(event);
+  // console.log('session', session);
+  if (!session) {
+    throw redirect(303, "/login")
+  }
   return {
-    session: await getServerSession(event),
+    session: session,
+    // session: await getServerSession(event),
   }
 }
